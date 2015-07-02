5ef2033d8e531f134f808c3524bf0dbbca7cde8d
Sachin Rudraraju <sachin.rudraraju@gmail.com>
2015-07-02 00:14:35 -0400

    works finally

--- chatter.py
+++ chatter.py
@@ -7,19 +7,27 @@ import sys
 from urllib import parse
 from datetime import datetime, timedelta
 import threading
+import subprocess
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+root = '/home/sachin/drive/projects/chatter/'
 
 url = "https://textyserver.appspot.com/"
-headers = {
-  "origin" : "https://mightytext.net",
-  "accept-enoding" : "gzip, deflate",
-  "accept-language" : "en-US,en;q=0.8",
-  "user-agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.125 Safari/537.36",
-  "content-type" : "application/x-www-form-urlencoded; charset=UTF-8",
-  "accept" : "application/json, text/javascript, */*; q=0.01",
-  "referer" : "https://mightytext.net/web8/?exp=1",
-  "authority" : "textyserver.appspot.com",
-  "Content-Length" : "0"}
-headers['cookie'] = open('cookie').read()
+headers={
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate",
+        "accept-language": "en-US,en;q=0.8",
+        "authority": "textyserver.appspot.com",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "origin": "https://mightytext.net",
+        "referer": "https://mightytext.net/web8/?exp=1",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.125 Safari/537.36",
+        "Content-Length" : "0"}
+    
+headers['cookie'] = open(root+'cookie').read()
 selfnum = '6143903969'
 # clean = lambda n: re.sub('(^\+|[^0-9]|\|)','', re.sub('\^+1','',n))
 clean = lambda n: re.sub('[^0-9\|]','', re.sub('(\+1|1 )','',n))
@@ -34,6 +42,7 @@ def update_messages():
   global messages
   try:
     r = json.loads(requests.get(url=url+'api', headers=headers, params={'function':'GetMessages', 'start_range':0,'end_range':100}).text)
+    #print(r['messages'][0])
     if 'user' in r: return messages
   except: return []
   
@@ -44,14 +53,15 @@ def update_messages():
         new.reverse()
         new = [{'body'  : parse.unquote_plus(m['body']),
                 'id'    : m['id'],
-                'time'  : (datetime.strptime(m['ts_carrier'], "%b %d, %Y %I:%M:%S %p") - timedelta(hours=4)) \
+                'time'  : (datetime.strptime(m['ts_server'], "%b %d, %Y %I:%M:%S %p") - timedelta(hours=4)) \
                             .strftime("%m/%d/%y %I:%M:%S").split(),
                 'phone' : clean(m['phone_num']),
+                'clean' : m['phone_num_clean'],
                 'read'  : m['is_read'],
                 'sent'  : (m['inbox_outbox'] == 61),
                 } for m in new]
         messages.extend(new)
-        with open('messages', 'w') as f: f.write(json.dumps(messages))
+        with open(root+'messages', 'w') as f: f.write(json.dumps(messages))
         return new
     except:
         print(r)
@@ -63,27 +73,38 @@ def get_contacts():
   except: return {}
   print('\n'.join([c['phoneList'][0]['phoneNumber'] for c in r]))
   contacts = {clean(c['phoneList'][0]['phoneNumber']):c['displayName'] for c in r}
-  with open('contacts', 'w') as f: f.write(json.dumps(contacts))
+  with open(root+'contacts', 'w') as f: f.write(json.dumps(contacts))
   return contacts
 
 def send(text, number):
-    print(number)
-    res = (requests.post(url=url+'client', headers=headers, 
-      data={'function':'send', 'deviceType':'ac2dm', 'source_client':'31', 'type':10, 'deviceType':'ac2dm','action':'send_sms',
-        'action_data' : text,
-        'phone'       : number,
-        }))
-    return res.status_code
+    # res = (requests.post(url=url+'client?function=send&deviceType=ac2dm&source_client=31', headers=headers, 
+    #   data={
+    #       'function':'send', 'deviceType':'ac2dm', 'source_client':'31', 
+    #       'type':10, 'deviceType':'ac2dm','action':'send_sms',
+    #     'action_data' : text,
+    #     'phone'       : ,
+    #     })
+    # res =  subprocess.check_output([root+'send.sh', \
+    #        parse.quote_plus('({}) {}-{}'.format(number[:3], number[3:6], number[6:])), 
+    #        parse.quote_plus(text)], shell=True)
+    res = subprocess.check_output(['/bin/sh', root+'/send.sh', 
+                    parse.quote_plus('+1({}) {}-{}'.format(number[:3], number[3:6], number[6:])), 
+                    # parse.quote_plus('+({}) {}-{}'.format(number[:3], number[3:6], number[6:])), 
+                    parse.quote_plus(text)])
+
+    print(res)
+    return 200
 
 def say(m):
   global contacts
-  old = datetime.now() - datetime.strptime(' '.join(m['time']), "%m/%d/%y %I:%M:%S") > timedelta(1)
+  old = datetime.now() - datetime.strptime(' '.join(m['time']), "%m/%d/%y %H:%M:%S") > timedelta(1)
   name = ', '.join([namer(contacts[p]) if p in contacts else p for p in m['phone'].split('|')])
   print('[\033[{}m{}\033[0m] [\033[{}m{}\033[0m] {}'.format(
+    #'90',
     '91' if m['sent'] else '95',
     m['time'][0] if old else m['time'][1], 
     #90 + (sum([ord(c) for c in name])%7),
-    '90',
+    90,
     name,
     #contacts[m['phone']] if len(m['phone']) == 10 else ', '.join(contacts[p] for p in m['phone'].split('|')),
     m['body']))
@@ -96,20 +117,22 @@ def sender(recp=None):
   global mlock
   rcontacts = {v: k for k, v in contacts.items()}
   with mlock:
-    ms = [m for m in messages if ((m['phone'] == rcontacts[recp]) if recp else True)][-25:]
+    ms = [m for m in messages if ((m['phone'] == rcontacts[recp]) if recp else True)][:]
     for m in ms: say(m)
 
   while 1:
     print('[\033[{}m{}\033[0m] '.format('96', recp if recp else "--------"), end="")
     tosend = input()
     if tosend.startswith('\\'):
-      recp = tosend[1:] if tosend[1:] in rcontacts else recp
-    else:
+      r = [r for r in rcontacts.keys() if r.startswith(tosend[1:])]
+      recp = r[0] if len(r) > 0 else recp
+      print('\n' in tosend)
+    elif recp and '\\' not in tosend:
       if len(tosend.strip()) > 0:
         res = send(tosend, rcontacts[recp])
         if res is not 200:
           print('[\033[{}m{}\033[0m]\n{}'.format(91, 'ERROR', 'STAUTS CODE: {}'.format(res)))
-        else: print('good')
+        else: pass
           
     with mlock:
       sending = True
@@ -134,10 +157,10 @@ def updater():
     time.sleep(delay)
 
 if __name__ == '__main__':
-  with open('contacts') as f: contacts = json.loads(f.read())
+  with open(root+'contacts') as f: contacts = json.loads(f.read())
   if len(contacts) == 0: contacts = get_contacts()
-  with open('messages') as f: messages = json.loads(f.read())
-  with open('/home/sachin/drive/people', 'w') as f: f.write('\n'.join(contacts.values()))
+  with open(root+'messages') as f: messages = json.loads(f.read())
+  with open('/home/sachin/drive/notes/people', 'w') as f: f.write('\n'.join(contacts.values()))
   person = ' '.join(sys.argv[1:])
   thread = threading.Thread(target=updater)
   thread.daemon = True
